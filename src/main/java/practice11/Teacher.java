f23812f39111e272b525933d7eaeb1309a23f0a5
Ruifeng-Wu <296078293@qq.com>
2019-03-28 18:59:24 +0800

    feat:implement practice11 test 11

--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,21 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> linkedList;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        linkedList = new LinkedList<>();
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.linkedList = linkedList;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
+    }
 }
