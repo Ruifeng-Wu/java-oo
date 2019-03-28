6d531c72902cce62adb9f2be8be48971a42b87e3
Ruifeng-Wu <296078293@qq.com>
2019-03-28 13:03:25 +0800

    refactor:refactor pratice07 Teacher class

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -23,9 +23,21 @@ public class Teacher extends Person {
     @Override
     public String introduce() {
         if (klass == null) {
-            return super.introduce() + " I am a Teacher. I teach No Class.";
+            return selfIntroduction() + " I teach No Class.";
         } else {
-            return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+            return selfIntroduction() + " I teach " + klass.getDisplayName() + ".";
+        }
+    }
+
+    public String selfIntroduction() {
+        return super.introduce() + " I am a Teacher.";
+    }
+
+    public String introduceWith(Student jerry) {
+        if (klass.getNumber() == jerry.getKlass().getNumber()) {
+            return selfIntroduction() + " I teach " + jerry.getName() + ".";
+        } else {
+            return selfIntroduction() + " I don't teach " + jerry.getName() + ".";
         }
     }
 }
