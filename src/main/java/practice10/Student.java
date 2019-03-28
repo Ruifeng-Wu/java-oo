0b2b3738402de05e89c9af789430d0dece121b95
Ruifeng-Wu <296078293@qq.com>
2019-03-28 18:05:36 +0800

    feat:implement practice10 test 9 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,6 +1,6 @@
 package practice10;
 
-public class Student extends Person{
+public class Student extends Person {
     private Klass klass;
 
     public Student(int id, String name, int age, Klass klass) {
@@ -18,5 +18,18 @@ public class Student extends Person{
 
     public void setKlass(Klass klass) {
         this.klass = klass;
+    }
+
+    public String basicInfo() {
+        return super.introduce() + " I am a Student.";
+    }
+
+    @Override
+    public String introduce() {
+        if (this.equals(klass.getLeader())) {
+            return basicInfo() + " I am Leader of " + klass.getDisplayName() + ".";
+        } else {
+            return basicInfo() + " I am at " + klass.getDisplayName() + ".";
+        }
     }
 }
