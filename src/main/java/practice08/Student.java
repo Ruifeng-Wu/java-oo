88260557258591c915b1225ae033b35749d068ea
Ruifeng-Wu <296078293@qq.com>
2019-03-28 13:28:18 +0800

    refactor:refactor pratice08 Student class

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -3,8 +3,21 @@ package practice08;
 public class Student extends Person {
     private Klass klass;
 
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
     public Student(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student."+" I am at "+klass.getDisplayName()+".";
     }
 }
