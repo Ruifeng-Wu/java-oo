bcec5959e4a108e36ff11def575811b68f2ca169
Ruifeng-Wu <296078293@qq.com>
2019-03-28 14:01:44 +0800

    feat:implement pratice08 test 4

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -6,4 +6,12 @@ public class Klass {
     public Klass(int number) {
         this.number = number;
     }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
