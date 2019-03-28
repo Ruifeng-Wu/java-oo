ef42a4ef4701ffab8754c9fb9fefec8669bcd246
Ruifeng-Wu <296078293@qq.com>
2019-03-28 14:31:28 +0800

    feat:implement pratice08 test 6 7

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,10 +1,17 @@
 package practice09;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Klass {
     private int number;
+    private Person leader;
+    private Map<Integer,Student> studentList;
+
 
     public Klass(int number) {
         this.number = number;
+        studentList=new HashMap<>();
     }
 
     public int getNumber() {
@@ -16,6 +23,22 @@ public class Klass {
     }
 
     public String getDisplayName() {
-        return "Class "+number;
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student jerry) {
+        if (!studentList.containsKey(jerry.getId())){
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        leader=jerry;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student){
+        studentList.put(student.getId(),student);
     }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,25 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+    private Student leader;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public void assignLeader(Student student) {
+        leader = student;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.equals(klass.getLeader())) {
+            return super.introduce() + " I am Leader of " + klass.getDisplayName() + ".";
+        } else {
+            return super.introduce() + " I am at " + klass.getDisplayName() + ".";
+        }
+
+    }
 }
