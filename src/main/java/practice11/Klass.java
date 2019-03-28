f8b98a84bc082e1cfc923dd71602e955083cca3f
Ruifeng-Wu <296078293@qq.com>
2019-03-28 18:47:40 +0800

    feat:implement practice11 test 6

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,10 +1,17 @@
 package practice11;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Klass {
     private int number;
+    private Person leader;
+    private Map<Integer, Student> studentList;
 
     public Klass(int number) {
         this.number = number;
+        studentList = new HashMap<>();
     }
 
     public int getNumber() {
@@ -13,5 +20,21 @@ public class Klass {
 
     public String getDisplayName() {
         return "Class " + number;
+    }
+
+    public void assignLeader(Student jerry) {
+        if (!studentList.containsKey(jerry.getId())) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        leader = jerry;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Person leader) {
+        this.leader = leader;
     }
 }
