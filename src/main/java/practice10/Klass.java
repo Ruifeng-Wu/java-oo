75d282e32f887e47808ad4289de44f31435e5f88
Ruifeng-Wu <296078293@qq.com>
2019-03-28 17:55:30 +0800

    feat:implement practice10 test 4

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,47 @@
 package practice10;
 
+import practice09.Person;
+import practice09.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Klass {
+    private int number;
+    private practice09.Person leader;
+    private Map<Integer,practice09.Student> studentList;
+
+
+    public Klass(int number) {
+        this.number = number;
+        studentList=new HashMap<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(practice09.Student jerry) {
+        if (!studentList.containsKey(jerry.getId())){
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        leader=jerry;
+    }
+
+    public Person getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student){
+        studentList.put(student.getId(),student);
+    }
 }
