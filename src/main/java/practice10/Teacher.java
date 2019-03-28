d8560c2066fbe657aa2d857acec722757df50932
Ruifeng-Wu <296078293@qq.com>
2019-03-28 18:24:26 +0800

    feat:implement practice10 test 12 13

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -2,16 +2,47 @@ package practice10;
 
 import java.util.LinkedList;
 
-public class Teacher extends Person{
+public class Teacher extends Person {
     private LinkedList<Klass> linkedList;
 
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.linkedList = new LinkedList<>();
+    }
 
     public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
         super(id, name, age);
         this.linkedList = linkedList;
     }
 
+    public LinkedList<Klass> getLinkedList() {
+        return linkedList;
+    }
+
+    public void setLinkedList(LinkedList<Klass> linkedList) {
+        this.linkedList = linkedList;
+    }
+
     public LinkedList getClasses() {
         return linkedList;
+    }
+
+    @Override
+    public String introduce() {
+
+        if (linkedList.isEmpty()) {
+            return basicInfo() + " I teach No Class.";
+        } else {
+            String temp=basicInfo()+" I teach Class "+linkedList.get(0).getNumber();
+            for (int i = 1; i < linkedList.size(); i++) {
+                temp+=(", "+linkedList.get(i).getNumber());
+            }
+            return temp+".";
+        }
+
+    }
+
+    public String basicInfo() {
+        return super.introduce() + " I am a Teacher.";
     }
 }
